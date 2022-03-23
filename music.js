5ad762cc10877170add5b1f04e0562955f3fb1d6
RoshanlalPrajapati <rooshanlal1200@gmail.com>
2022-03-23 10:56:24 +0530

    Updates

---
+++ music.js
@@ -0,0 +1,62 @@
+let allMusic = [
+    {
+        name: "Dhadak - Title Song",
+        artist: "Ajay Atul, Shreya Ghosal",
+        img: "photo_2022-01-23_19-30-52.jpg",
+        src: "01 Dhadak - Title Song.mp3"
+    },
+    {
+        name: "Tu Itni Khoobsurat Hai",
+        artist: "Rahat Fathe Ali Khan",
+        img: "wallpaperflare.com_wallpaper (1).jpg",
+        src: "02 Tu Itni Khoobsurat Hai - Barkhaa (Rahat Fateh Ali Khan) 190Kbps.mp3"
+    },
+    {
+        name: "Tu Dua Hai Dua",
+        artist: "Mohd, Irfan",
+        img: "wallpaperflare.com_wallpaper (2).jpg",
+        src: "07 Tu Dua Hai Dua (Ishq Ne Krazy Kiya Re).mp3"
+    },
+    {
+        name: "Blackpink_How do you like that",
+        artist: "Lisa, Rose",
+        img: "wallpaperflare.com_wallpaper (3).jpg",
+        src: "BLACKPINK_-__How_You_Like_That__M_V(256k).mp3"
+    },
+    {
+        name: "Chaha_Hai_Tujhko_Song",
+        artist: "Unknown Artist",
+        img: "WhatsApp Image 2020-12-08 at 1.00.09 PM (1).jpeg",
+        src: "Chaha_Hai_Tujhko_Song_Cover_By_Debolinaa_Nandy__Mann__Aamir_Khan,_Manisha__Old_Songs_Renditions(256k).mp3"
+    },
+    {
+        name: "Chand_Sifarish",
+        artist: "Unknown Artist",
+        img: "WhatsApp Image 2020-12-08 at 1.00.09 PM (2).jpeg",
+        src: "Chand_Sifarish_-_Full_Song___Fanaa___Aamir_Khan___Kajol___Shaan___Kailash_Kher.mp3"
+    },
+    {
+        name: "Dil Meri Na Sune",
+        artist: "Unknown Artist",
+        img: "WhatsApp Image 2020-12-08 at 1.00.09 PM (3).jpeg",
+        src: "Dil Meri Na Sune Mp3 - Atif Aslam 320kbps(Mp3go.in).mp3"
+    },
+    {
+        name: "dil_sambhal_ja_zara",
+        artist: "Unknown Artist",
+        img: "WhatsApp Image 2020-12-08 at 1.00.09 PM.jpeg",
+        src: "dil_sambhal_ja_zara_phr_mhbt.FLV.mp3"
+    },
+    {
+        name: "Hare_Hare_Hare_Hum_To_Dil_Se_Hare",
+        artist: "Unknown Artist",
+        img: "wp5208815-itachi-aesthetic-ps4-wallpapers.jpg",
+        src: "Hare_Hare_Hare_Hum_To_Dil_Se_Hare_Song_Ringtone_Tik_Tok_Trending_Ringtone_New_Trend_Ringtone(256k).mp3"
+    },
+    {
+        name: "Luka_Chuppi",
+        artist: "Unknown Artist",
+        img: "wallpaperflare.com_wallpaper (2).jpg",
+        src: "Luka_Chuppi__Photo_Song___Kartik_Aaryan,_Kriti_Sanon___Karan_S___Goldboy___Tanis.mp3"
+    }
+]
